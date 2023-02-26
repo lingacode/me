06b22dcdc41d4048be2ebdc4a87736203484f8d3
lingacode <lingarajpatra480@gmail.com>
2023-02-26 09:56:27 +0000

    hyhy

--- app.js
+++ app.js
@@ -16,3 +16,24 @@ btnShow.addEventListener('click', () => {
 function addZero(num){
     return num < 10 ? `0${num}`:num;
 }
+// calculater
+let string = "";
+let buttons =document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string=eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'c'){
+            string=" ";
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            string= string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
+// end calculater
