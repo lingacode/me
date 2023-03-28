58a9e259f45f34909b2f5d44d984e5584fb0d609
lingacode <lingarajpatra480@gmail.com>
2023-03-28 14:46:32 +0000

    hhch

--- me.js
+++ me.js
@@ -10,3 +10,25 @@ function myFunction(){
         navbar_header.classList.remove("sticky");
     }
 }
+/* <---------------------------calculater---------------------------------> */
+
+let string = "";
+let buttons =document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string=eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'c'){
+            string=" ";
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            string= string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
+/* <---------------------------calculater end---------------------------------> */
